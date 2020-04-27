0a35a12e2c87e284f54ec55ff6dcf8b65c11fc35
JoacoMontandon <JOAQUIN.MONTANDON@correo.ucu.edu.uy>
2020-04-27 10:39:13 -0300

    Agrego más Tests

--- src/Test/Library.Test/UnitTest1.cs
+++ src/Test/Library.Test/UnitTest1.cs
@@ -90,5 +90,84 @@ namespace Library.Test
 
             Assert.AreEqual(100,knight.Cure());
         }
+        [Test]
+        public void TestknightEquipAttackAndUnEquipAttackItem()
+        {
+            Knight knight = new Knight("Knight");
+            Axe axe = new Axe();
+            Bow bow = new Bow();
+            knight.EquipAttackItem(axe);
+            knight.EquipAttackItem(bow);
+
+            Assert.AreEqual(25+15,knight.GetTotalAttackValue());
+
+            knight.UnEquipAttackItem(axe);
+            knight.UnEquipAttackItem(bow);
+
+            Assert.AreEqual(0,knight.GetTotalAttackValue());
+        }
+        public void TestWizzardEquipAttackAndUnEquipAttackItem()
+        {
+            Wizard gandalf = new Wizard("Gandalf");
+            Staff staff = new Staff();
+            Bow bow = new Bow();
+            gandalf.EquipMixedItem(staff);
+            gandalf.EquipAttackItem(bow);
+
+            Assert.AreEqual(100+15,gandalf.GetTotalAttackValue());
+
+            gandalf.UnEquipMixedItem(staff);
+            gandalf.UnEquipAttackItem(bow);
+
+            Assert.AreEqual(0,gandalf.GetTotalAttackValue());
+        }
+        [Test]
+        public void TestArcherEquipAttackAndUnEquipAttackItem()
+        {
+            Archer archer = new Archer("Archer");
+            Axe axe = new Axe();
+            Bow bow = new Bow();
+            archer.EquipAttackItem(axe);
+            archer.EquipAttackItem(bow);
+
+            Assert.AreEqual(25+15,archer.GetTotalAttackValue());
+
+            archer.UnEquipAttackItem(axe);
+            archer.UnEquipAttackItem(bow);
+
+            Assert.AreEqual(0,archer.GetTotalAttackValue());
+        }
+        [Test]
+        public void TestDwarfEquipAttackAndUnEquipAttackItem()
+        {
+            Dwarf dwarf = new Dwarf("Dwarf");
+            Axe axe = new Axe();
+            Bow bow = new Bow();
+            dwarf.EquipAttackItem(axe);
+            dwarf.EquipAttackItem(bow);
+
+            Assert.AreEqual(25+15,dwarf.GetTotalAttackValue());
+
+            dwarf.UnEquipAttackItem(axe);
+            dwarf.UnEquipAttackItem(bow);
+
+            Assert.AreEqual(0,dwarf.GetTotalAttackValue());
+        }
+        [Test]
+        public void TestEquipDefensiveItemAndUnEquip()
+        {
+            Wizard gandalf = new Wizard("Gandalf");
+            Staff staff = new Staff();
+            Shield shield = new Shield();
+            gandalf.EquipMixedItem(staff);
+            gandalf.EquipDefensiveItem(shield);
+
+            Assert.AreEqual(100+14,gandalf.GetTotalDefenseValue());
+
+            gandalf.UnEquipMixedItem(staff);
+            gandalf.UnEquipDefensiveItem(shield);
+
+            Assert.AreEqual(0,gandalf.GetTotalDefenseValue());
+        }
     }
 }
