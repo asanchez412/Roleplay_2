c99f169541adb7aa88cc3a84522c53401582120c
asanchez412 <sanchezandres41997@gmail.com>
2020-05-08 16:58:27 -0300

    Add TestsEncuentros

---
+++ src/Test/Library.Test/TestsEncuentros.cs
@@ -0,0 +1,200 @@
+using NUnit.Framework;
+using RoleplayGame;
+
+namespace Library.Test
+
+{
+    public class TestsEncuentros
+    {
+
+        [Test]
+        public void TestEncuentroCompleto()
+        {
+            Renegade renegade = new Renegade("Renegade");
+            Ogre ogre = new Ogre("Ogre");
+            Giant giant = new Giant("Giant");
+
+            Dwarf gimli = new Dwarf("Gimli");
+
+            Helmet helmet = new Helmet();
+            Sword sword = new Sword();
+
+            gimli.EquipItem(helmet);
+            gimli.EquipItem(sword);
+
+            Encuentro encuentro = new Encuentro();
+
+            encuentro.AddHero(gimli);
+
+            encuentro.AddEnemy(renegade);
+            encuentro.AddEnemy(ogre);
+            encuentro.AddEnemy(giant);
+
+            Assert.AreEqual(0, 0);
+        }
+
+        [Test]
+        public void TestEnemiesAttackOneHero()
+        {
+            Renegade renegade = new Renegade("Renegade");
+            Ogre ogre = new Ogre("Ogre");
+            Giant giant = new Giant("Giant");
+
+            Dwarf gimli = new Dwarf("Gimli");
+
+            Helmet helmet = new Helmet();
+            Sword sword = new Sword();
+
+            gimli.EquipItem(helmet);
+            gimli.EquipItem(sword);
+
+            Encuentro encuentro = new Encuentro();
+
+            encuentro.AddHero(gimli);
+
+            encuentro.AddEnemy(renegade);
+            encuentro.AddEnemy(ogre);
+            encuentro.AddEnemy(giant);
+
+            Assert.AreEqual(0, 0);
+        }
+
+        public void TestEnemiesAttackLessHeroes()
+        {
+            Renegade renegade = new Renegade("Renegade");
+            Ogre ogre = new Ogre("Ogre");
+            Giant giant = new Giant("Giant");
+
+            Dwarf gimli = new Dwarf("Gimli");
+            Knight knight = new Knight("Knight");
+
+            Helmet helmet = new Helmet();
+            Sword sword = new Sword();
+
+            gimli.EquipItem(helmet);
+            gimli.EquipItem(sword);
+
+            knight.EquipItem(helmet);
+            knight.EquipItem(sword);
+
+            Encuentro encuentro = new Encuentro();
+
+            encuentro.AddHero(gimli);
+            encuentro.AddHero(knight);
+
+            encuentro.AddEnemy(renegade);
+            encuentro.AddEnemy(ogre);
+            encuentro.AddEnemy(giant);
+
+            Assert.AreEqual(0, 0);
+        }
+
+        [Test]
+        public void TestEnemiesAttackEqualHeroes()
+        {
+            Renegade renegade = new Renegade("Renegade");
+            Ogre ogre = new Ogre("Ogre");
+            Giant giant = new Giant("Giant");
+
+            Dwarf gimli = new Dwarf("Gimli");
+            Knight knight = new Knight("Knight");
+
+            Helmet helmet = new Helmet();
+            Sword sword = new Sword();
+
+            gimli.EquipItem(helmet);
+            gimli.EquipItem(sword);
+
+            knight.EquipItem(helmet);
+            knight.EquipItem(sword);
+
+            Encuentro encuentro = new Encuentro();
+
+            encuentro.AddHero(gimli);
+            encuentro.AddHero(knight);
+
+            encuentro.AddEnemy(renegade);
+            encuentro.AddEnemy(ogre);
+            encuentro.AddEnemy(giant);
+
+            Assert.AreEqual(0, 0);
+        }
+
+        [Test]
+        public void TestHeroesAttack()
+        {
+            Renegade renegade = new Renegade("Renegade");
+            Ogre ogre = new Ogre("Ogre");
+            Giant giant = new Giant("Giant");
+
+            Knight knight = new Knight("Knight");
+
+            Helmet helmet = new Helmet();
+            Sword sword = new Sword();
+
+            knight.EquipItem(helmet);
+            knight.EquipItem(sword);
+
+            Encuentro encuentro = new Encuentro();
+
+            encuentro.AddHero(knight);
+
+            encuentro.AddEnemy(renegade);
+            encuentro.AddEnemy(ogre);
+            encuentro.AddEnemy(giant);
+
+            Assert.AreEqual(0, 0);
+        }
+
+        [Test]
+        public void TestCureHeroEnoughVP()
+        {
+            Renegade renegade = new Renegade("Renegade");
+            Ogre ogre = new Ogre("Ogre");
+            Giant giant = new Giant("Giant");
+
+            Knight knight = new Knight("Knight");
+
+            Helmet helmet = new Helmet();
+            Sword sword = new Sword();
+
+            knight.EquipItem(helmet);
+            knight.EquipItem(sword);
+
+            Encuentro encuentro = new Encuentro();
+
+            encuentro.AddHero(knight);
+
+            encuentro.AddEnemy(renegade);
+            encuentro.AddEnemy(ogre);
+            encuentro.AddEnemy(giant);
+
+            Assert.AreEqual(0, 0);
+        }
+
+        public void TestCureHeroNotEnoughVP()
+        {
+            Renegade renegade = new Renegade("Renegade");
+            Ogre ogre = new Ogre("Ogre");
+            Giant giant = new Giant("Giant");
+
+            Knight knight = new Knight("Knight");
+
+            Helmet helmet = new Helmet();
+            Sword sword = new Sword();
+
+            knight.EquipItem(helmet);
+            knight.EquipItem(sword);
+
+            Encuentro encuentro = new Encuentro();
+
+            encuentro.AddHero(knight);
+
+            encuentro.AddEnemy(renegade);
+            encuentro.AddEnemy(ogre);
+            encuentro.AddEnemy(giant);
+
+            Assert.AreEqual(0, 0);
+        }
+    }
+}
